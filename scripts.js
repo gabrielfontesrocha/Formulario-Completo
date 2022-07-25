b9d03d9dfaa381656eb8f88714baf9e9e07339c0
Gabriel <gabrielfontesrocha1@gmail.com>
2022-07-25 20:11:26 -0300

    .

---
+++ scripts.js
@@ -0,0 +1,13 @@
+const form = document.getElementById('form')
+const username = document.getElementById('username')
+const email = document.getElementById('email')
+const password = document.getElementById('password')
+const passwordConfirmation = document.getElementById('password-confirmation')
+
+form.addEventListener('submit', (e) = {
+    e.preventDefault();
+});
+
+function checkInputs() {
+    
+}
